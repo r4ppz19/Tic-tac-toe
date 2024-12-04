b44d6d1f15428823c75ef48cd348914d68dabd84
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-05 00:28:44 +0800

    Fix image icon loading in Main.java to ensure resource availability and maintain application consistency

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage mainStage) throws Exception {
         // Load the application icon
-        Image imageIcon = new Image("/images/r4ppzIcon.png");
+        Image imageIcon = new Image(Objects.requireNonNull(getClass().getResource("/images/r4ppzIcon.png")).toExternalForm());
 
         // Load the FXML layout for the main view
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainView.fxml")));
@@ -29,9 +29,9 @@ public class Main extends Application {
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/MainDesign.css")).toExternalForm());
 
         // Set the application icon and scene to the main stage
-        mainStage.setResizable(false);
         mainStage.getIcons().add(imageIcon);
         mainStage.setScene(scene);
+        mainStage.setResizable(false);
         mainStage.show(); // Display the main stage
     }
 }
